f5b9af32787e24670934a0ebe5d0257a63fc5ea8
Your Name <you@example.com>
2025-03-10 02:46:46 +0200

    Assignment3

--- Assignment_EF/Data/ITI_Context.cs
+++ Assignment_EF/Data/ITI_Context.cs
@@ -20,32 +20,6 @@ namespace Assignment03_EF.Data
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
             base.OnModelCreating(modelBuilder);
-
-            //modelBuilder.Entity<Student>()
-            //            .HasMany(s => s.stud_Courses)
-            //            .WithOne(sc => sc.Student)
-            //            .HasForeignKey(sc => sc.stud_ID);
-
-            //modelBuilder.Entity<Course>()
-            //            .HasMany(c => c.stud_Courses)
-            //            .WithOne(sc => sc.Course)
-            //            .HasForeignKey(sc => sc.Course_ID);
-
-            //modelBuilder.Entity<Instructor>()
-            //            .HasMany(i => i.course_Insts)
-            //            .WithOne(sc => sc.instructor)
-            //            .HasForeignKey(sc => sc.inst_ID);
-
-            //modelBuilder.Entity<Course>()
-            //            .HasMany(c => c.course_Insts)
-            //            .WithOne(sc => sc.course)
-            //            .HasForeignKey(sc => sc.Course_ID);
-
-            //modelBuilder.Entity<Course_Inst>()
-            //            .HasKey(ci => new { ci.Course_ID, ci.inst_ID });
-
-            //modelBuilder.Entity<Stud_Course>()
-            //            .HasKey(sc => new { sc.Course_ID, sc.stud_ID });
         }
         public DbSet<Course_Inst> course_Insts { get; set; }
         public DbSet<Stud_Course> stud_Courses { get; set; }
