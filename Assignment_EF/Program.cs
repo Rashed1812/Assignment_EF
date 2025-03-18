0aa59ef9243320db9c4d4e496934a188fe52940c
Your Name <you@example.com>
2025-03-18 03:16:57 +0200

    Assignment05

--- Assignment_EF/Program.cs
+++ Assignment_EF/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Assignment03_EF;
 using Assignment03_EF.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace Assignment_EF
 {
@@ -10,76 +11,123 @@ namespace Assignment_EF
         {
             using ITI_Context dbcontext = new ITI_Context();
 
-            #region Session 4
-            //var st = (from e in dbcontext.Students
-            //          where e.Id ==101
-            //          select e).FirstOrDefault();
-            //Console.WriteLine($"Student Name: {st.FName} \nAge:{st.Age}\nAddress:{st.Address}\nDeptid:{st.Dept_Id}");
+            #region Session 5
 
-            //var dep = (from d in dbcontext.Departments
-            //           where d.Id==3
-            //           select d).FirstOrDefault();
-            //Console.WriteLine($"Department Name: {dep.Name}");
-            //foreach (var st in dep.Students)
+            #region Inner Join
+            //1.Query syntax
+            //var result = from S in dbcontext.Students
+            //             join D in dbcontext.Departments
+            //             on S.Dept_Id equals D.Id
+            //             select new
+            //             {
+            //                 StudentName = S.FName,
+            //                 DeptName = D.Name 
+            //             };
+
+            //2.Fluent Syntax
+            //var result = dbcontext.Students.Join(dbcontext.Departments, s => s.Dept_Id, d => d.Id,
+            //                                     (s, d) => new
+            //                                     {
+            //                                         StudentName = s.FName,
+            //                                         DeptName = d.Name
+            //                                     }).Where(a=>a.DeptName=="cs");
+            //foreach (var item in result)
             //{
-            //    Console.WriteLine($"Student Name:{st.FName}");
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region Group Join
+
+            #region ex1
+            //var result = dbcontext.Departments.GroupJoin(dbcontext.Students, d => d.Id, s => s.Dept_Id, (d, s) => new
+            //{
+            //    Department = d,
+            //    Student = s
+            //});
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine($"{item.Department.Name}");
+            //    Console.WriteLine("--------------------------------------------------");
+            //    foreach (var item1 in item.Student)
+            //    {
+            //        Console.WriteLine(item1.FName);
+            //    }
+            //} 
+            #endregion
+
+            #region ex2
+
+            //var result = dbcontext.Departments.GroupJoin(dbcontext.Students, d => d.Id, s => s.Dept_Id, (Department, Student) => new
+            //{
+            //    Department = Department,
+            //    Student = Student
+            //}).Where(A=>A.Student.Count()>1);
+
+            //foreach (var Item in result)
+            //{
+            //    Console.WriteLine(Item.Department.Name);
+            //    Console.WriteLine("---------------------------------------------------");
+            //    foreach (var stItem in Item.Student)
+            //    {
+            //        Console.WriteLine(stItem.FName);
+            //    }
+            //}
+            #endregion
+
+            #region Left Join Not Working
+            //var result = dbcontext.Departments.LeftJoin(dbcontext.Students, d => d.Id, s => s.Dept_Id, (d, s) => new
+            //{
+            //    Department = d,
+            //    Student = s
+            //});
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
             //} 
 
             #endregion
 
-            #region Explicit loading
-            //var st = (from e in dbcontext.Students
-            //           where e.Id ==101
-            //           select e).FirstOrDefault();
-
-            //dbcontext.Entry(st).Reference(nameof(st.Department)).Load();
-            //dbcontext.Entry(st).Collection(nameof(st.Department)).Load();
-            //Console.WriteLine($"Department Name: {st.Department.Name}");
-
-            #endregion
-
-            #region Eager Loading
-
-            //var st = (from e in dbcontext.Students.Include(e=>e.Department)
-            //           where e.Id ==101
-            //           select e).FirstOrDefault();
-            //Console.WriteLine($"Department Name: {st.Department.Name}");
-
-            #endregion
-
-            #region Lazy Loading
-
-            //var st = (from e in dbcontext.Students
-            //          where e.Id ==101
-            //          select e).FirstOrDefault();
-
-            //Console.WriteLine($"Department Name: {st?.Department.Name}");
-
-            #endregion
-
-            #region Tracking Not Tracking
-
-            ////dbcontext.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
-            //var st = (from e in dbcontext.Students
-            //          where e.Id ==101
-            //          select e).FirstOrDefault();
-
-
-            //Console.WriteLine(dbcontext.Entry(st).State);
-            //st.FName = "Ahmed";
-            //Console.WriteLine(dbcontext.Entry(st).State);
-
-            //dbcontext.SaveChanges(); 
-            #endregion
-
-            #region Mapping View
-
-            ////var result = dbcontext.studentDepartments.FromSqlRaw("select * from StudentDepartmentView");
-            
-            //foreach (var item in dbcontext.studentDepartments)
+            #region Right Outer Join
+            //var result = dbcontext.Students.GroupJoin(dbcontext.Departments,s => s.Dept_Id, d=>d.Id, (s, d) => new
             //{
-            //    Console.WriteLine($"StudentFName: {item.FirstName} StudentLName: {item.LastName} DepartmentId: {item.DepartmentId} DepartmentName: {item.DepartmentName}");
+            //    Department = d,
+            //    Student = s
+            //});
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item.Student.FName);
+
+            //    Console.WriteLine("-----------------------------------");
+            //    foreach (var item1 in item.Department)
+            //    {
+            //        Console.WriteLine(item1.Name);
+            //    }
+            //} 
+            #endregion
+
+            #endregion
+
+            #region Cross Join
+            //var result = from s in dbcontext.Students
+            //             from d in dbcontext.Departments
+            //             select new
+            //             {
+            //                 s.FName,
+            //                 deptName = d.Name
+            //             };
+
+            //var result = dbcontext.Students.SelectMany(s=> dbcontext.Departments.Select(d=>new 
+            //{
+            //   s.FName,
+            //   DeptName = d.Name
+            //}));
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
             //}
+            #endregion
 
             #endregion
 
